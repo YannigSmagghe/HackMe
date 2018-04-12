3a698553ddb4e11899da7180582007a9425854fe
mariecgs <marie.cogis@gmail.com>
2018-04-12 10:43:13 +0200

    style

--- search.php
+++ search.php
@@ -1,6 +1,8 @@
 <html>
 <head>
     <title>Search book</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -11,21 +13,33 @@ include_once("config.php");
 $books = mysqli_query($mysqli, "SELECT id, denomination FROM comics;")
 
 ?>
+<div class="container-fluid top">
+    <ul>
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+    </ul>
+</div>
+<div class="container bc-secondaire">
+    <h1>Rechercher un livre.</h1>
 
-<h1>Rechercher un livre.</h1>
-
-<form action="search.php" method="post">
-    <select name="books" id="select_book">
-        <?php
-        while($row = $books->fetch_array()){
-            ?>
-            <option value="<?= $row['id']  ?>"><?= $row['denomination'] ?></option>
-            <?php
-        }
-        ?>
-    </select>
-    <input type="submit" value="Valider">
-</form>
+    <form action="search.php" method="post">
+        <div class="row">
+            <select name="books" id="select_book">
+                <?php
+                while($row = $books->fetch_array()){
+                    ?>
+                    <option value="<?= $row['id']  ?>"><?= $row['denomination'] ?></option>
+                    <?php
+                }
+                ?>
+            </select>
+        </div>
+        <div class="row submit">
+            <input type="submit" value="Valider">
+        </div>
+    </form>
+</div>
 
 
 <?php
